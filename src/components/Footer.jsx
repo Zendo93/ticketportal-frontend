09949cc85daaa76c158b65b24a0ee2eb87611be3
Richard Uzsak <Richard.Uzsak@aomvsr.sk>
2024-03-09 16:33:03 +0100

    init

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <footer className="mx-auto flex p-6 lg:px-8 bg-sky-100" aria-label="Global">
+            <div className="flex lg:flex-1">
+                <span className="text-sm
+                     text-gray-500 
+                     sm:text-center
+                      dark:text-gray-400">
+                        © 2024 Richard Uzsák
+                </span>
+            </div>
+        </footer>
+    );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+      <header className="App-header">
+        <nav className="mx-auto flex p-6 lg:px-8 bg-sky-100" aria-label="Global">
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <img className="h-8 w-auto" 
+                src="logo-ticketportal-2023-blue.svg" 
+                alt="Ticketportal-logo" />
+            </a>
+          </div>
+        </nav>
+      </header>
+    )
+  }
