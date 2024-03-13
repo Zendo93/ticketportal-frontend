47a1017f2d0cb99817fe3f1f209539697e8432c3
Richard Uzsak <Richard.Uzsak@aomvsr.sk>
2024-03-13 13:25:42 +0100

    added tooltip to seats

--- src/components/EventEdit/EventEdit.jsx
+++ src/components/EventEdit/EventEdit.jsx
@@ -53,25 +53,67 @@ export default function EventEdit() {
             <div>
                 <hr class="w-100 h-1 mx-auto my-4 bg-gray-500 border-0 rounded md:my-10 dark:bg-gray-700" />
                 <div className="inline-flex flex-wrap justify-between">
-                    <div class="box-border flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                    <div class="box-border group relative flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                        <div class="absolute bottom-[calc(100%+0.5rem)] left-[50%] -translate-x-[50%] hidden group-hover:block w-auto">
+                            <div class="bottom-full right-0 rounded bg-black px-4 py-1 text-xs text-white whitespace-nowrap">
+                                Tooltip center
+                                <svg class="absolute left-0 top-full h-2 w-full text-black" x="0px" y="0px" viewBox="0 0 255 255" /*xml:space="preserve"*/><polygon class="fill-current" points="0,0 127.5,127.5 255,0" /></svg>
+                            </div>
+                        </div>
                         <div class="m-auto">5</div>
                     </div>
-                    <div class="box-border flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                    <div class="box-border group relative flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                        <div class="absolute bottom-[calc(100%+0.5rem)] left-[50%] -translate-x-[50%] hidden group-hover:block w-auto">
+                            <div class="bottom-full right-0 rounded bg-black px-4 py-1 text-xs text-white whitespace-nowrap">
+                                Tooltip center
+                                <svg class="absolute left-0 top-full h-2 w-full text-black" x="0px" y="0px" viewBox="0 0 255 255" /*xml:space="preserve"*/><polygon class="fill-current" points="0,0 127.5,127.5 255,0" /></svg>
+                            </div>
+                        </div>
                         <div class="m-auto">5</div>
                     </div>
-                    <div class="box-border flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                    <div class="box-border group relative flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                        <div class="absolute bottom-[calc(100%+0.5rem)] left-[50%] -translate-x-[50%] hidden group-hover:block w-auto">
+                            <div class="bottom-full right-0 rounded bg-black px-4 py-1 text-xs text-white whitespace-nowrap">
+                                Tooltip center
+                                <svg class="absolute left-0 top-full h-2 w-full text-black" x="0px" y="0px" viewBox="0 0 255 255" /*xml:space="preserve"*/><polygon class="fill-current" points="0,0 127.5,127.5 255,0" /></svg>
+                            </div>
+                        </div>
                         <div class="m-auto">5</div>
                     </div>
-                    <div class="box-border flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                    <div class="box-border group relative flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                        <div class="absolute bottom-[calc(100%+0.5rem)] left-[50%] -translate-x-[50%] hidden group-hover:block w-auto">
+                            <div class="bottom-full right-0 rounded bg-black px-4 py-1 text-xs text-white whitespace-nowrap">
+                                Tooltip center
+                                <svg class="absolute left-0 top-full h-2 w-full text-black" x="0px" y="0px" viewBox="0 0 255 255" /*xml:space="preserve"*/><polygon class="fill-current" points="0,0 127.5,127.5 255,0" /></svg>
+                            </div>
+                        </div>
                         <div class="m-auto">5</div>
                     </div>
-                    <div class="box-border flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                    <div class="box-border group relative flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                        <div class="absolute bottom-[calc(100%+0.5rem)] left-[50%] -translate-x-[50%] hidden group-hover:block w-auto">
+                            <div class="bottom-full right-0 rounded bg-black px-4 py-1 text-xs text-white whitespace-nowrap">
+                                Tooltip center
+                                <svg class="absolute left-0 top-full h-2 w-full text-black" x="0px" y="0px" viewBox="0 0 255 255" /*xml:space="preserve"*/><polygon class="fill-current" points="0,0 127.5,127.5 255,0" /></svg>
+                            </div>
+                        </div>
                         <div class="m-auto">5</div>
                     </div>
-                    <div class="box-border flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                    <div class="box-border group relative flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                        <div class="absolute bottom-[calc(100%+0.5rem)] left-[50%] -translate-x-[50%] hidden group-hover:block w-auto">
+                            <div class="bottom-full right-0 rounded bg-black px-4 py-1 text-xs text-white whitespace-nowrap">
+                                Tooltip center
+                                <svg class="absolute left-0 top-full h-2 w-full text-black" x="0px" y="0px" viewBox="0 0 255 255" /*xml:space="preserve"*/><polygon class="fill-current" points="0,0 127.5,127.5 255,0" /></svg>
+                            </div>
+                        </div>
                         <div class="m-auto">5</div>
                     </div>
-                    <div class="box-border flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                    <div class="box-border group relative flex border-gray-500 h-16 w-16 p-4 border-4 my-2">
+                        <div class="absolute bottom-[calc(100%+0.5rem)] left-[50%] -translate-x-[50%] hidden group-hover:block w-auto">
+                            <div class="bottom-full right-0 rounded bg-black px-4 py-1 text-xs text-white whitespace-nowrap">
+                                Tooltip center
+                                <svg class="absolute left-0 top-full h-2 w-full text-black" x="0px" y="0px" viewBox="0 0 255 255" /*xml:space="preserve"*/><polygon class="fill-current" points="0,0 127.5,127.5 255,0" /></svg>
+                            </div>
+                        </div>
                         <div class="m-auto">5</div>
                     </div>
                 </div>
